d1a7e2cc581b3c137cfd4a73c24eb4fe2a3ac7a6
Nicholas Rixson <44293604+segcore@users.noreply.github.com>
2026-01-05 21:56:46 +0800

    Remove redundant &&

--- compile_all.sh
+++ compile_all.sh
@@ -4,9 +4,8 @@ jai -x64 test.jai -quiet
 ./test
 (
     cd examples
-    jai -x64 compiletime.jai -quiet &&
-    jai -x64 pregenerate.jai -quiet &&
-    ./pregenerate &&
-    jai -x64 use_generated.jai -quiet &&
-    :
+    jai -x64 compiletime.jai -quiet
+    jai -x64 pregenerate.jai -quiet
+    ./pregenerate
+    jai -x64 use_generated.jai -quiet
 )
